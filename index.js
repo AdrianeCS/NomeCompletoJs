6865d0fa2aa2e9c5fa464f81ae172f1536fbe35c
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-11-10 22:16:50 -0300

    fonalizar

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let nome = prompt("Digite seu primeiro nome");
 let sobrenome = prompt("digite seu sobrenome");
 
-let nomecompleto = nome = sobrenome;
 
-alert(`Seu nome completo é: ${nome + " " + sobrenome} \nSeu nome de catálogo é: ${sobrenome.toUpperCase()} ${nome}`);
+
+alert(`Seu nome completo é: ${nome + " " + sobrenome} \nSeu nome de catálogo é: ${sobrenome.toUpperCase()}, ${nome}`);
